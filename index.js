6ec55c2509fa687b9f45f584126e5fc327b7875f
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-08-21 01:06:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight(){
+     const leftNumbers = dodger.style.left.replace("px","");
+     const left = parseInt(leftNumbers, 10);
+     const dodgerWidth = dodger.offsetWidth;
+     const windowWidth = window.innerWidth;
+     const maxRightPosition = windowWidth - dodgerWidth;
+
+     if (left < maxRightPosition) {
+        dodger.style.left = `${left + 1}px`;
+     }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+    });
+
+  
